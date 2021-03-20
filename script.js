22dbca0a50ab862d017f4942ff7268853863b0db
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-20 18:12:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 'use strict';
-//Selecting Elements
-const playerOneScoreTot = document.querySelector('#score--0');
-const playerTwoScoreTot = document.querySelector('#score--1');
-const diceImgHidden = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
+//Grabbing the HTML elements
+const overallScore1 = document.getElementById('score--0');
+const overallScore2 = document.getElementById('score--1');
+const activePlayerOne = document.getElementById('current--0');
+const activePlayerTwo = document.getElementById('current--1');
+const diceImg = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-const activatePlayer = document.querySelector('.player--active');
+const newGame = document.querySelector('.btn--new');
+const hold = document.querySelector('.btn--hold');
 
-//Starting Conditions
-playerOneScoreTot.textContent = 0;
-playerTwoScoreTot.textContent = 0;
-diceImgHidden.classList.add('hidden');
+//Start/ Reset of Game
+overallScore1.textContent = 0;
+overallScore2.textContent = 0;
+diceImg.classList.add('hidden');
 
-//Rolling the Dice
+let currentScore = 0;
+
+//Rolling dice
+
 btnRoll.addEventListener('click', function () {
-  // Random Dice Roll
-  const dice = Math.trunc(Math.random() * 6) + 1; //Truncating with Math.random method
-
-  // Displaying Dice - Removing Hidden Class
-  diceImgHidden.classList.remove('hidden');
-
-  //Maniputlating the src of the HTML to display the correct image.
-  diceImgHidden.src = `dice-${dice}.png`;
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceImg.classList.remove('hidden');
+  diceImg.src = `dice-${dice}.png`;
   console.log(dice);
-
-  // If a 1 is rolled, switch to player 2
   if (dice !== 1) {
+    currentScore += dice;
+    activePlayerOne.textContent = currentScore; //Change later
   } else {
   }
 });
