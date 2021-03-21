aae52107bb84de0dd760cfef6963b8983a977caa
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-20 19:24:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 //Grabbing the HTML elements
+const playerZeroBG = document.querySelector('.player--0');
+const playerOneBG = document.querySelector('.player--1');
 const overallScore1 = document.getElementById('score--0');
 const overallScore2 = document.getElementById('score--1');
 const activePlayerOne = document.getElementById('current--0');
@@ -7,14 +9,16 @@ const activePlayerTwo = document.getElementById('current--1');
 const diceImg = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const newGame = document.querySelector('.btn--new');
-const hold = document.querySelector('.btn--hold');
+const btnHold = document.querySelector('.btn--hold');
 
 //Start/ Reset of Game
 overallScore1.textContent = 0;
 overallScore2.textContent = 0;
 diceImg.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice
 
@@ -22,10 +26,17 @@ btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceImg.classList.remove('hidden');
   diceImg.src = `dice-${dice}.png`;
-  console.log(dice);
+
   if (dice !== 1) {
     currentScore += dice;
-    activePlayerOne.textContent = currentScore; //Change later
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    playerZeroBG.classList.toggle('player--active');
+    playerOneBG.classList.toggle('player--active');
   }
 });
